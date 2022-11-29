958ea8f25f7a62ad7b04a7680ed30b7906738bd1
sebleur <sebastien.leurquin@std.heh.be>
2022-11-29 10:16:12 +0100

    first commit

---
+++ src/main/java/be/heh/ec/projetepm/ProjetEpmApplication.java
@@ -0,0 +1,13 @@
+package be.heh.ec.projetepm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetEpmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/heh/ec/projetepm/ProjetEpmApplicationTests.java
@@ -0,0 +1,13 @@
+package be.heh.ec.projetepm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEpmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
